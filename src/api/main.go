e8e447ddfaaeb39282dd82043cde7c202ceea12b
frederickmannings <fred.mannings@predixus.com>
2024-03-06 12:17:20 +0000

    Moved API code into library

--- src/api/main.go
+++ src/api/main.go
@@ -1,7 +1,14 @@
 package api
 
 import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"os/signal"
+	"time"
 
 	"github.com/gorilla/mux"
 
@@ -20,4 +27,55 @@ func GenerateRouter() *mux.Router {
 	r.HandleFunc("/register-epoch", epoch.EpochHandler)
 
 	return r
+}
+
+func StartHTTPServer() {
+	// Start the HTTP API
+	var wait time.Duration
+	flag.DurationVar(
+		&wait,
+		"graceful-timeout",
+		time.Second*15,
+		"The duration for which the server gracefull waits for existing connections to finish.",
+	)
+	flag.Parse()
+
+	// Route definitions
+	r := GenerateRouter()
+	port := os.Getenv("API_PORT")
+	if port == "" {
+		port = "4040"
+	}
+	srv := &http.Server{
+		Addr:         fmt.Sprintf("localhost:%s", port),
+		WriteTimeout: time.Second * 15,
+		ReadTimeout:  time.Second * 15,
+		IdleTimeout:  time.Second * 60,
+		Handler:      r,
+	}
+
+	// setup logging
+	go func() {
+		if err := srv.ListenAndServe(); err != nil {
+			log.Println(err)
+		}
+	}()
+
+	// create a channel to listen for interrupts
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+
+	// block until the signal is recieved
+	<-c
+
+	// create a deadline
+	// this will not block if there are no connections
+	// if there are, it will wait untill the timeout completes
+	ctx, cancel := context.WithTimeout(context.Background(), wait)
+	defer cancel()
+
+	srv.Shutdown(ctx)
+
+	log.Println("Shutting Down")
+	os.Exit(0)
 }
