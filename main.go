4a1a27de99ad9b5a13a433db1aaf40275745d04a
frederickmannings <fred.mannings@solvicode.com>
2024-05-06 11:44:18 +0100

    Intermediate work

--- main.go
+++ main.go
@@ -1,48 +1,9 @@
 package main
 
 import (
-	"flag"
-	"sync"
-
-	api "github.com/predixus/analytics_framework/internal/api"
-	datalayer "github.com/predixus/analytics_framework/internal/datalayer"
-	provision "github.com/predixus/analytics_framework/internal/datalayer/provision"
-	grpc "github.com/predixus/analytics_framework/internal/grpc"
-	li "github.com/predixus/analytics_framework/internal/logger"
+	"github.com/predixus/analytics_framework/internal/setup"
 )
 
 func main() {
-	// first check whether there are any command line arguments
-	initPtr := flag.Bool("init-db", false, "Provision the local postgres db")
-	flag.Parsed()
-
-	if *initPtr {
-		println("Initialising postgres DB")
-		err := provision.Provision()
-		if err != nil {
-			li.Logger.Fatal(err)
-		}
-		return
-	}
-
-	// connect to the postgres store
-	datalayer.ConnectDB()
-	defer datalayer.StorageDB.Close()
-
-	// start a waitgroup
-	var wg sync.WaitGroup
-
-	// Start the gRPC framework server
-	wg.Add(1)
-	go func() {
-		grpc.StartGRPCServer(&wg)
-	}()
-
-	// Start the API - TODO: kick out into seperate module
-	wg.Add(1)
-	go func() {
-		api.StartHTTPServer(&wg)
-	}()
-
-	wg.Wait()
+	setup.Setup()
 }
