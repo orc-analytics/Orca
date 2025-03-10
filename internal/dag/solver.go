18b0d455afbd54ad66be55ca1253058e82d5d927
Frederick Mannings <fred.mannings@solvicode.com>
2025-03-10 10:07:58 +0000

    Incremental

---
+++ internal/dag/solver.go
@@ -0,0 +1,10 @@
+package dag
+
+type DAG struct{}
+
+// SolveDag - perform the dag solving activity
+// given a set of algorithms, their triggering windows, processors
+// and dependencies, find the execution paths broken down by processor
+func SolveDag(dependencies map[string]string) (*DAG, error) {
+	return nil, nil
+}
