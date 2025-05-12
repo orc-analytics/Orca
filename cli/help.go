845a34b8805be4b35a2f1bd1231c9071064b65bd
Frederick Mannings <fred.mannings@solvicode.com>
2025-05-12 08:46:45 +0100

    Added CLI styling

--- cli/help.go
+++ cli/help.go
@@ -3,46 +3,60 @@ package main
 import "fmt"
 
 func showHelp() {
-	fmt.Println("Orca CLI")
-	fmt.Println("\nUsage:")
-	fmt.Println("  command [options]")
-	fmt.Println("\nCommands:")
-	fmt.Println("  start   - Start the Orca stack")
-	fmt.Println("  stop    - Stop the Orca stack")
-	fmt.Println("  status  - Show status of the Orca components")
-	fmt.Println("  destroy - Delete all Orca resources")
-	fmt.Println("  help    - Show this help message or help for a specific command")
+	fmt.Println(headerStyle.Render("Orca CLI"))
 
-	fmt.Println("\nExamples:")
-	fmt.Println("  start")
-	fmt.Println("  stop")
-	fmt.Println("  status")
-	fmt.Println("  destroy")
-	fmt.Println("  help start")
+	fmt.Println(subHeaderStyle.Render("\nUsage:"))
+	fmt.Println(infoStyle.Render("  command [options]"))
+
+	fmt.Println(subHeaderStyle.Render("\nCommands:"))
+	fmt.Println(infoStyle.Render("  start   ") + "- Start the Orca stack")
+	fmt.Println(infoStyle.Render("  stop    ") + "- Stop the Orca stack")
+	fmt.Println(infoStyle.Render("  status  ") + "- Show status of the Orca components")
+	fmt.Println(infoStyle.Render("  destroy ") + "- Delete all Orca resources")
+	fmt.Println(
+		infoStyle.Render("  help    ") + "- Show this help message or help for a specific command",
+	)
+
+	fmt.Println(subHeaderStyle.Render("\nExamples:"))
+	fmt.Println(infoStyle.Render("  orca start"))
+	fmt.Println(infoStyle.Render("  orca stop"))
+	fmt.Println(infoStyle.Render("  orca status"))
+	fmt.Println(infoStyle.Render("  orca destroy"))
+	fmt.Println(infoStyle.Render("  orca help start"))
 }
 
 func showCommandHelp(command string) {
 	switch command {
 	case "start":
-		fmt.Println("'start' command - Start the Orca stack")
-		fmt.Println("\nUsage:")
-		fmt.Println("  start")
-		fmt.Println("\nExample:")
-		fmt.Println("  start")
+		fmt.Println(subHeaderStyle.Render("'start' command - Start the Orca stack"))
+		fmt.Println(infoStyle.Render("\nUsage:"))
+		fmt.Println(infoStyle.Render("  orca start"))
+		fmt.Println(infoStyle.Render("\nExample:"))
+		fmt.Println(infoStyle.Render("  orca start"))
+
 	case "stop":
-		fmt.Println("'stop' command - Stop the Orca stack")
-		fmt.Println("\nUsage:")
-		fmt.Println("  stop")
-		fmt.Println("\nExample:")
-		fmt.Println("  stop")
+		fmt.Println(subHeaderStyle.Render("'stop' command - Stop the Orca stack"))
+		fmt.Println(infoStyle.Render("\nUsage:"))
+		fmt.Println(infoStyle.Render("  orca stop"))
+		fmt.Println(infoStyle.Render("\nExample:"))
+		fmt.Println(infoStyle.Render("  orca stop"))
+
 	case "status":
-		fmt.Println("'status' command - Show status of the Orca Services")
-		fmt.Println("\nUsage:")
-		fmt.Println("  status")
-		fmt.Println("\nExamples:")
-		fmt.Println("  status")
+		fmt.Println(subHeaderStyle.Render("'status' command - Show status of the Orca services"))
+		fmt.Println(infoStyle.Render("\nUsage:"))
+		fmt.Println(infoStyle.Render("  orca status"))
+		fmt.Println(infoStyle.Render("\nExample:"))
+		fmt.Println(infoStyle.Render("  orca status"))
+
+	case "destroy":
+		fmt.Println(subHeaderStyle.Render("'destroy' command - Tear down the Orca environment"))
+		fmt.Println(infoStyle.Render("\nUsage:"))
+		fmt.Println(infoStyle.Render("  orca destroy"))
+		fmt.Println(infoStyle.Render("\nExample:"))
+		fmt.Println(infoStyle.Render("  orca destroy"))
+
 	default:
-		fmt.Printf("Unknown command: %s\n", command)
+		fmt.Println(errorStyle.Render(fmt.Sprintf("Unknown command: %s", command)))
 		showHelp()
 	}
 }
